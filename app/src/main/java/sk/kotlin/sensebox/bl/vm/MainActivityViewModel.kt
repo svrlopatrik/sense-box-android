a2000951d73d526f0d63e925df46b9c7254033d5
Patrik Svrlo <svrlopatrik@gmail.com>
2018-09-15 16:01:37 +0200

    MainActivity screen implementation

--- app/src/main/java/sk/kotlin/sensebox/bl/vm/MainActivityViewModel.kt
+++ app/src/main/java/sk/kotlin/sensebox/bl/vm/MainActivityViewModel.kt
@@ -1,14 +1,48 @@
 package sk.kotlin.sensebox.bl.vm
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.bluetooth.BluetoothProfile
 import android.os.Bundle
+import io.reactivex.schedulers.Schedulers
+import sk.kotlin.sensebox.bl.bt.BleClient
+import sk.kotlin.sensebox.models.states.MainActivityState
 import javax.inject.Inject
 
 /**
  * Created by Patrik Švrlo on 8.9.2018.
  */
-class MainActivityViewModel @Inject constructor() : BaseViewModel() {
+class MainActivityViewModel @Inject constructor(
+        private val bleClient: BleClient
+) : BaseViewModel() {
+
+    private val mainActivityState = MutableLiveData<MainActivityState>()
 
     override fun onViewCreated(savedInstanceState: Bundle?) {
-
+        if (savedInstanceState == null) {
+            addDisposable(bleClient.connectionState()
+                    .subscribeOn(Schedulers.newThread())
+                    .subscribe(
+                            {
+                                mainActivityState.postValue(when (it) {
+                                    BluetoothProfile.STATE_CONNECTING -> MainActivityState.BleConnecting
+                                    BluetoothProfile.STATE_CONNECTED -> MainActivityState.BleConnected
+                                    BluetoothProfile.STATE_DISCONNECTING -> MainActivityState.BleDisconnecting
+                                    BluetoothProfile.STATE_DISCONNECTED -> MainActivityState.BleDisconnected
+                                    else -> MainActivityState.Error(Throwable("Unknown ble state"))
+                                })
+                            },
+                            { mainActivityState.postValue(MainActivityState.Error(it)) }
+                    )
+            )
+        }
     }
+
+    override fun onCleared() {
+        super.onCleared()
+        bleClient.disconnect()
+        bleClient.releaseConnection()
+    }
+
+    fun getMainActivityState(): LiveData<MainActivityState> = mainActivityState
 }
--- app/src/main/java/sk/kotlin/sensebox/ui/activities/main/MainActivity.kt
+++ app/src/main/java/sk/kotlin/sensebox/ui/activities/main/MainActivity.kt
@@ -1,12 +1,19 @@
 package sk.kotlin.sensebox.ui.activities.main
 
+import android.Manifest
+import android.arch.lifecycle.Observer
 import android.content.Context
 import android.content.Intent
+import android.databinding.ObservableInt
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
+import sk.kotlin.sensebox.BR
 import sk.kotlin.sensebox.R
 import sk.kotlin.sensebox.bl.vm.MainActivityViewModel
+import sk.kotlin.sensebox.models.states.MainActivityState
 import sk.kotlin.sensebox.ui.activities.BaseActivity
 import sk.kotlin.sensebox.ui.fragments.history.HistoryFragment
 import sk.kotlin.sensebox.ui.fragments.live.LiveFragment
@@ -15,21 +22,62 @@ import sk.kotlin.sensebox.ui.fragments.settings.SettingsFragment
 /**
  * Created by Patrik Švrlo on 8.9.2018.
  */
-class MainActivity : BaseActivity<MainActivityViewModel>(MainActivityViewModel::class.java) {
+class MainActivity : BaseActivity<MainActivityViewModel>(MainActivityViewModel::class.java), EasyPermissions.PermissionCallbacks {
 
     private lateinit var navigationPagerAdapter: NavigationPagerAdapter
 
     companion object {
+        const val RC_ALLOW_BLUETOOTH = 0x01
+
         fun startActivity(context: Context) {
             context.startActivity(Intent(context, MainActivity::class.java))
         }
     }
 
+    val bleStateImageSrc = ObservableInt(R.drawable.ic_bluetooth_disconnected)
+    val bleStateImageTintColor = ObservableInt(R.color.cc_rd_800)
+
     override fun setLayout() = R.layout.activity_main
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        checkPermissions(RC_ALLOW_BLUETOOTH, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
     override fun initViews(savedInstanceState: Bundle?) {
+        viewBinding?.apply {
+            setVariable(BR.activity, this@MainActivity)
+        }
+
+        observeState()
         initViewPager()
         initBottomNavigation()
+    }
+
+    private fun observeState() {
+        viewModel?.let { viewModel ->
+            viewModel.getMainActivityState().observe(this, Observer { state -> state?.let { render(it) } })
+        }
+    }
+
+    private fun render(state: MainActivityState) {
+        when (state) {
+            is MainActivityState.BleConnecting -> {
+                bleStateImageSrc.set(R.drawable.ic_bluetooth_searching)
+                bleStateImageTintColor.set(R.color.cc_yw_800)
+            }
+            is MainActivityState.BleConnected -> {
+                bleStateImageSrc.set(R.drawable.ic_bluetooth_connected)
+                bleStateImageTintColor.set(R.color.cc_gn_800)
+            }
+            is MainActivityState.BleDisconnected -> {
+                bleStateImageSrc.set(R.drawable.ic_bluetooth_disconnected)
+                bleStateImageTintColor.set(R.color.cc_rd_800)
+            }
+            is MainActivityState.Error -> {
+
+            }
+        }
     }
 
     private fun initViewPager() {
@@ -52,6 +100,33 @@ class MainActivity : BaseActivity<MainActivityViewModel>(MainActivityViewModel::
                 R.id.action_settings -> view_pager.setCurrentItem(2, true)
             }
             true
+        }
+    }
+
+    private fun checkPermissions(requestCode: Int, vararg permissions: String): Boolean {
+        if (!EasyPermissions.hasPermissions(this, *permissions)) {
+            EasyPermissions.requestPermissions(this, getString(R.string.permissions_rationale), requestCode, *permissions)
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        when (requestCode) {
+            RC_ALLOW_BLUETOOTH -> {
+            }
         }
     }
 
