b6415b17c6917a6a7124ff605ee61d5a50faa63a
Patrik Svrlo <svrlopatrik@gmail.com>
2018-09-24 20:19:20 +0200

    implementation of settings screen in progress

--- app/src/main/java/sk/kotlin/sensebox/bl/vm/SettingsFragmentViewModel.kt
+++ app/src/main/java/sk/kotlin/sensebox/bl/vm/SettingsFragmentViewModel.kt
@@ -1,14 +1,43 @@
 package sk.kotlin.sensebox.bl.vm
 
+import android.arch.lifecycle.LiveData
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import sk.kotlin.sensebox.Constants
+import sk.kotlin.sensebox.bl.PreferencesManager
+import sk.kotlin.sensebox.utils.SingleLiveEvent
 import javax.inject.Inject
 
 /**
  * Created by Patrik Švrlo on 8.9.2018.
  */
-class SettingsFragmentViewModel @Inject constructor() : BaseViewModel() {
+class SettingsFragmentViewModel @Inject constructor(
+        private val preferences: PreferencesManager
+) : BaseViewModel() {
+
+    private val temperatureUnit = SingleLiveEvent<String>()
 
     override fun onViewCreated(savedInstanceState: Bundle?) {
-
+        temperatureUnit.value = PreferencesManager.getStringValue(PreferencesManager.PreferenceKey.TEMPERATURE_SYMBOL)
     }
+
+    fun onTemperatureUnitChanged(radioGroup: RadioGroup, id: Int) {
+        val text = radioGroup.findViewById<RadioButton>(id).text.toString()
+        if (text != temperatureUnit.value) {
+            temperatureUnit.value = text
+            preferences.Builder()
+                    .setString(PreferencesManager.PreferenceKey.TEMPERATURE_SYMBOL, text)
+                    .setByte(PreferencesManager.PreferenceKey.TEMPERATURE_UNIT, getTemperatureUnitFromSymbol(text))
+                    .store()
+        }
+    }
+
+    private fun getTemperatureUnitFromSymbol(symbol: String) = when (symbol) {
+        "°C" -> Constants.UNIT_FLAG_TEMPERATURE_CELSIUS
+        "°F" -> Constants.UNIT_FLAG_TEMPERATURE_FAHRENHEIT
+        else -> throw Exception("undefined temperature symbol")
+    }
+
+    fun getTemperatureUnit(): LiveData<String> = temperatureUnit
 }
--- app/src/main/java/sk/kotlin/sensebox/ui/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/sk/kotlin/sensebox/ui/fragments/settings/SettingsFragment.kt
@@ -2,6 +2,7 @@ package sk.kotlin.sensebox.ui.fragments.settings
 
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import sk.kotlin.sensebox.BR
 import sk.kotlin.sensebox.R
 import sk.kotlin.sensebox.bl.vm.SettingsFragmentViewModel
 import sk.kotlin.sensebox.ui.fragments.BaseFragment
@@ -22,7 +23,9 @@ class SettingsFragment : BaseFragment<SettingsFragmentViewModel>() {
     override fun setViewModel() = ViewModelProviders.of(this, viewModelFactory).get(SettingsFragmentViewModel::class.java)
 
     override fun initViews(savedInstanceState: Bundle?) {
-
+        viewBinding?.apply {
+            setVariable(BR.viewModel, viewModel)
+        }
     }
 
 }
